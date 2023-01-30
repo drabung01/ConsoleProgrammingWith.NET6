6d87c71ce22b6763dbfd90decf8f60152570acfc
drabung01 <rabungd@gmail.com>
2023-01-30 14:11:33 -0500

    Adding in an attempt at Exception Handling

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,11 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Enter Length:");
-int length = Convert.ToInt32(Console.ReadLine());
+int length, width, height;
+
+try
+{
+    Console.WriteLine("Enter Length:");
+    length = Convert.ToInt32(Console.ReadLine());
+}
+catch (System.FormatException)
+{
+    Console.WriteLine("Your entry was not a valid numerical input. Try again.");
+    Console.WriteLine("Enter Length:");
+    length = Convert.ToInt32(Console.ReadLine());
+}
+catch (System.OverflowException)
+{
+    Console.WriteLine("Your entry was too large of a number. Try again.");
+    Console.WriteLine("Enter Length:");
+    length = Convert.ToInt32(Console.ReadLine());
+}
+
+
 Console.WriteLine("Enter Width:");
-int width = Convert.ToInt32(Console.ReadLine());
+width = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter Height:");
-int height = Convert.ToInt32(Console.ReadLine());
+height = Convert.ToInt32(Console.ReadLine());
 
 Cube cube = new Cube(width, height, length);
 var triangle = new Triangle(length, height, 27);
